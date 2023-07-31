01d6d8ec5a55901725f9fdcf1cba6ad294c2d065
CarlosMunoz <jose.munoz2@coxautoinc.com>
2023-07-31 12:11:10 -0600

    First endpoint to check health

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("world");
+    format!("Hello, {}", &name)
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    println!("Server running at http://0.0.0.0:9090");
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("0.0.0.0:9090")?
+        .run()
+        .await
 }
