80392456801bc3b47951050151b980d9c86951a3
CarlosMunoz <jose.munoz2@coxautoinc.com>
2023-07-31 12:35:23 -0600

    First test that will be unable to test if another framework is used

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("world");
-    format!("Hello, {}", &name)
-}
-
-async fn health_check() -> impl Responder {
+//async fn health_check() -> impl Responder {
+async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
@@ -16,4 +12,19 @@ async fn main() -> Result<(), std::io::Error> {
         .bind("0.0.0.0:9090")?
         .run()
         .await
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::health_check;
+
+    #[tokio::test]
+    async fn health_check_succeds() {
+        let response = health_check().await;
+
+        // this requires chaging the response type to
+        // HttpResponse to compile
+        // Need to importit with `use actix_web::HttpResponse`
+        assert!(response.status().is_success())
+    }
 }
